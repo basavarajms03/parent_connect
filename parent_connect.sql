a232e0097b6260cc9b5b1172853b78ec8d5a5a2e
Basavaraj Mallappa <basavaraj.mallappa@amazecodes.com>
2022-07-22 10:36:05 +0530

    SQL File has been updated

--- parent_connect.sql
+++ parent_connect.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 29, 2022 at 07:12 AM
+-- Generation Time: Jul 22, 2022 at 05:05 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -38,17 +38,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   PRIMARY KEY (`id`),
   KEY `student_id` (`student_id`),
   KEY `subjectId` (`subjectId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`id`, `subjectId`, `student_id`, `number_of_classes`, `no_of_cls_attended`, `attendance_type`) VALUES
-(1, '124', '123456', 10, 8, '1'),
-(2, '124', '123456', 15, 12, '2'),
-(3, '124', '123456', 14, 11, '3'),
-(4, '124', '123456', 14, 11, 'finalSem');
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -64,14 +54,15 @@ CREATE TABLE IF NOT EXISTS `departments` (
   `password` varchar(100) NOT NULL DEFAULT 'Password@123',
   PRIMARY KEY (`deptId`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `departments`
 --
 
 INSERT INTO `departments` (`id`, `deptId`, `department_name`, `password`) VALUES
-(2, '12345', 'Computer Science', 'Password@123');
+(2, '12345', 'Computer Science', 'Password@123'),
+(3, '1245', 'Mechanical Engineering', 'Password@123');
 
 -- --------------------------------------------------------
 
@@ -91,14 +82,14 @@ CREATE TABLE IF NOT EXISTS `faculties` (
   `password` varchar(100) NOT NULL DEFAULT 'Password@123',
   PRIMARY KEY (`id`),
   KEY `deptId` (`deptId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculties`
 --
 
 INSERT INTO `faculties` (`id`, `first_name`, `last_name`, `deptId`, `faculty_id`, `email`, `mobile`, `password`) VALUES
-(1, 'Basavaraj', 'Sangur', '12345', '1234', 'basavaraj@gmail.com', 9739170220, 'Password@123');
+(2, 'Basavaraj', 'Sangur', '12345', '1234', 'basavaraj@gmail.com', 9739170220, 'Password@123');
 
 -- --------------------------------------------------------
 
@@ -117,19 +108,14 @@ CREATE TABLE IF NOT EXISTS `marks_details` (
   PRIMARY KEY (`id`),
   KEY `subjectId` (`subjectId`),
   KEY `student_id` (`student_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `marks_details`
 --
 
 INSERT INTO `marks_details` (`id`, `subjectId`, `total_marks`, `obtained_marks`, `student_id`, `marks_type`) VALUES
-(1, '124', 123, 123, '123456', '1'),
-(2, '124', 1234, 1234, '123456', '2'),
-(3, '124', 12, 21, '123456', '3'),
-(4, '124', 32, 23, '123456', 'finalSem'),
-(5, '1234', 100, 40, '123456', '1'),
-(6, '125', 25, 20, '123456', '1');
+(27, '305IC', 30, 20, '202CS12013', '1');
 
 -- --------------------------------------------------------
 
@@ -154,14 +140,46 @@ CREATE TABLE IF NOT EXISTS `students_list` (
   PRIMARY KEY (`roll_no`) USING BTREE,
   UNIQUE KEY `id` (`id`),
   KEY `students_list_ibfk_1` (`dept_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `students_list`
 --
 
 INSERT INTO `students_list` (`id`, `roll_no`, `full_name`, `last_name`, `email_id`, `phone_number`, `father_name`, `mother_name`, `dept_id`, `semester`, `password`, `father_mobile_number`) VALUES
-(1, '123456', 'Basavaraj', 'Sangur', 'b@gmail.com', 97391702201, 'Mallappa', 'Laxmi', '12345', 1, 'Password@123', 9739170220);
+(4, '202CS12013', 'Basavaraj', 'Sangur', 'basavaraj@gmail.com', 1234567890, 'Mallappa', 'Laxmi', '12345', 1, 'Password@123', 973917);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_marks_details`
+--
+
+DROP TABLE IF EXISTS `student_marks_details`;
+CREATE TABLE IF NOT EXISTS `student_marks_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subjectId` varchar(100) NOT NULL,
+  `student_id` varchar(100) NOT NULL,
+  `CIE1` varchar(11) DEFAULT '0,0',
+  `CIE2` varchar(11) DEFAULT '0,0',
+  `CIE3` varchar(11) DEFAULT '0,0',
+  `MCQ` varchar(11) DEFAULT '0,0',
+  `OPENBOOKTEST` varchar(11) DEFAULT '0,0',
+  `activity` varchar(11) DEFAULT '0,0',
+  `SKTEST1` varchar(11) DEFAULT '0,0',
+  `SKTEST2` varchar(11) DEFAULT '0,0',
+  `portfolio` varchar(11) DEFAULT '0,0',
+  `SKTEST3` varchar(11) DEFAULT '0,0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_marks_details`
+--
+
+INSERT INTO `student_marks_details` (`id`, `subjectId`, `student_id`, `CIE1`, `CIE2`, `CIE3`, `MCQ`, `OPENBOOKTEST`, `activity`, `SKTEST1`, `SKTEST2`, `portfolio`, `SKTEST3`) VALUES
+(5, '203WE', '202CS12013', '30,28', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0'),
+(4, '201DC', '202CS12013', '30,28', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0');
 
 -- --------------------------------------------------------
 
@@ -179,16 +197,17 @@ CREATE TABLE IF NOT EXISTS `subjects` (
   PRIMARY KEY (`subjectId`) USING BTREE,
   UNIQUE KEY `id` (`id`) USING BTREE,
   KEY `subjects_ibfk_1` (`deptId`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subjects`
 --
 
 INSERT INTO `subjects` (`id`, `subjectId`, `subjectName`, `semester`, `deptId`) VALUES
-(3, '1234', 'Digital Computer', 2, '12345'),
-(1, '124', 'Cloud Computing', 1, '12345'),
-(18, '125', 'C++', 2, '12345');
+(22, '201DC', 'Digital Computer', 1, '12345'),
+(23, '202PY', 'Python', 2, '12345'),
+(24, '203WE', 'Web Programming', 3, '12345'),
+(25, '305IC', 'Introduction to computer', 5, '12345');
 
 --
 -- Constraints for dumped tables
