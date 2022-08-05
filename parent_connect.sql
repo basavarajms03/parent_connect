576d663aadb18da9967d0ff096f8404533840e88
Basavaraj Sangur <basavaraj.sangur03@outlook.com>
2022-08-05 11:35:22 +0530

    Changes to sql has completed

--- parent_connect.sql
+++ parent_connect.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2022 at 03:56 PM
+-- Generation Time: Aug 05, 2022 at 08:04 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -36,18 +36,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   PRIMARY KEY (`id`),
   KEY `student_id` (`student_id`),
   KEY `subjectId` (`subjectId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`id`, `subjectId`, `student_id`, `number_of_classes`, `no_of_cls_attended`, `attendance_type`) VALUES
-(1, '202PY', '202CS12013', 30, 28, 'January'),
-(2, '203WE', '202CS12013', 30, 26, 'January'),
-(3, '202PY', '202CS12013', 30, 22, 'February'),
-(4, '202PY', '202CS12013', 20, 12, 'March'),
-(5, '202PY', '202CS12013', 30, 27, 'April');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -69,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `departments` (
 --
 
 INSERT INTO `departments` (`id`, `deptId`, `department_name`, `password`) VALUES
-(2, '1234', 'Computer Science', '12345'),
+(2, '12345', 'Computer Science', '12345'),
 (3, '1245', 'Mechanical Engineering', 'Password@123');
 
 -- --------------------------------------------------------
@@ -88,15 +77,9 @@ CREATE TABLE IF NOT EXISTS `faculties` (
   `mobile` double NOT NULL,
   `password` varchar(100) NOT NULL DEFAULT 'Password@123',
   PRIMARY KEY (`id`),
-  KEY `deptId` (`deptId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
---
--- Dumping data for table `faculties`
---
-
-INSERT INTO `faculties` (`id`, `first_name`, `last_name`, `deptId`, `faculty_id`, `email`, `mobile`, `password`) VALUES
-(6, 'Basavaraj', 'S', '1234', '12345', 'basavaraj@gmail.com', 9739170220, 'Password@123');
+  KEY `deptId` (`deptId`),
+  KEY `faculty_id` (`faculty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -114,15 +97,7 @@ CREATE TABLE IF NOT EXISTS `marks_details` (
   PRIMARY KEY (`id`),
   KEY `subjectId` (`subjectId`),
   KEY `student_id` (`student_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
-
---
--- Dumping data for table `marks_details`
---
-
-INSERT INTO `marks_details` (`id`, `subjectId`, `total_marks`, `obtained_marks`, `student_id`, `marks_type`) VALUES
-(32, '201CS', -1, -1, '202CS12013', '1'),
-(43, '305IC', 30, 28, '202CS12013', '1');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -146,14 +121,7 @@ CREATE TABLE IF NOT EXISTS `students_list` (
   PRIMARY KEY (`roll_no`) USING BTREE,
   UNIQUE KEY `id` (`id`),
   KEY `students_list_ibfk_1` (`dept_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `students_list`
---
-
-INSERT INTO `students_list` (`id`, `roll_no`, `full_name`, `last_name`, `email_id`, `phone_number`, `father_name`, `mother_name`, `dept_id`, `semester`, `password`, `father_mobile_number`) VALUES
-(4, '202CS12013', 'Basavaraj', 'Sangur', 'basavaraj@gmail.com', 1234567890, 'Mallappa', 'Laxmi', '1234', 1, 'Password@123', 9739170220);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -176,15 +144,7 @@ CREATE TABLE IF NOT EXISTS `student_marks_details` (
   `portfolio` varchar(11) DEFAULT '0,0',
   `SKTEST3` varchar(11) DEFAULT '0,0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
-
---
--- Dumping data for table `student_marks_details`
---
-
-INSERT INTO `student_marks_details` (`id`, `subjectId`, `student_id`, `CIE1`, `CIE2`, `CIE3`, `MCQ`, `OPENBOOKTEST`, `activity`, `SKTEST1`, `SKTEST2`, `portfolio`, `SKTEST3`) VALUES
-(11, '203WE', '202CS12013', '30,28', '30,28', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0'),
-(10, '202PY', '202CS12013', '30,28', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0', '0,0');
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -198,20 +158,12 @@ CREATE TABLE IF NOT EXISTS `subjects` (
   `subjectName` varchar(100) NOT NULL,
   `semester` int(11) NOT NULL,
   `deptId` varchar(30) NOT NULL,
+  `facultyId` varchar(30) NOT NULL,
   PRIMARY KEY (`subjectId`) USING BTREE,
   UNIQUE KEY `id` (`id`) USING BTREE,
-  KEY `subjects_ibfk_1` (`deptId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
-
---
--- Dumping data for table `subjects`
---
-
-INSERT INTO `subjects` (`id`, `subjectId`, `subjectName`, `semester`, `deptId`) VALUES
-(26, '201CS', 'Software Engineering', 5, '1234'),
-(23, '202PY', 'Python', 3, '1234'),
-(24, '203WE', 'Web Programming', 3, '1234'),
-(25, '305IC', 'Introduction to computer', 5, '1234');
+  KEY `subjects_ibfk_1` (`deptId`),
+  KEY `facultyId` (`facultyId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
 
 -- --------------------------------------------------------
 
@@ -223,17 +175,10 @@ CREATE TABLE IF NOT EXISTS `subjects_count` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sem` int(11) NOT NULL,
   `total_subjects` int(11) NOT NULL,
-  `deptId` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `subjects_count`
---
-
-INSERT INTO `subjects_count` (`id`, `sem`, `total_subjects`, `deptId`) VALUES
-(2, 3, 2, 1234),
-(3, 5, 2, 1234);
+  `deptId` varchar(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `deptId` (`deptId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
@@ -270,6 +215,12 @@ ALTER TABLE `students_list`
 --
 ALTER TABLE `subjects`
   ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`deptId`) REFERENCES `departments` (`deptId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subjects_count`
+--
+ALTER TABLE `subjects_count`
+  ADD CONSTRAINT `subjects_count_ibfk_1` FOREIGN KEY (`deptId`) REFERENCES `departments` (`deptId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
